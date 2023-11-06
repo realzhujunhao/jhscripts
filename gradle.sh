fe38c3d50f1bacd3e1b89903bc9aa40c594f0d57
JunhaoZhu <realzhujunhao@gmail.com>
2023-11-06 19:33:29 +0900

    gradle

---
+++ gradle.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/zsh
+
+GRADLE_ROOT=/opt/gradle
+VERSION=8.4
+GRADLE_PATH=${GRADLE_ROOT}/${VERSION}
+ZIPNAME=gradle-${VERSION}-bin.zip
+
+sudo mkdir ${GRADLE_ROOT}
+cd ${GRADLE_ROOT}
+sudo wget https://gradle.org/next-steps/?version=${VERSION}&format=bin
+sudo unzip ${ZIPNAME}
+DIRNAME=$(ls -d */)
+sudo mv ${DIRNAME} ${VERSION}
+sudo rm ${ZIPNAME}
+
+cd ~
+echo "# Gradle" >> ~/.zshrc
+echo "GRADLE_HOME=${GRADLE_PATH}" >> ~/.zshrc
+echo "PATH=\$GRADLE_HOME/bin:\$PATH"
+source ~/.zshrc
