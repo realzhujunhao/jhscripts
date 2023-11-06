4c3cd94c87a76e8017ea05f45d8d2bcaf45bd799
JunhaoZhu <realzhujunhao@gmail.com>
2023-11-06 18:57:44 +0900

    partial nvim

---
+++ nvim.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+cd ~/
+git clone https://github.com/LuaJIT/LuaJIT.git
+cd ~/LuaJIT
+sudo make
+sudo make install
+
+NODE_VERSION=v21.1.0
+NODE_ROOT=/opt/nodejs
+NODE_PATH=${NODE_ROOT}/${NODE_VERSION}
+
+sudo mkdir ${NODE_ROOT}
+cd ${NODE_ROOT}
+
+PLATFORM=linux-x64
+
+sudo wget https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-${PLATFORM}.tar.xz
+sudo tar -xvf node-${NODE_VERSION}-${PLATFORM}.tar.xz
+sudo rm node-${NODE_VERSION}-${PLATFORM}.tar.xz
+sudo mv node-${NODE_VERSION}-${PLATFORM}/ ${NODE_VERSION}/
+
+cd ~/
+
+echo "export NODE_HOME=${NODE_PATH}" >> ~/.zshrc
+echo "export PATH=\$NODE_HOME/bin:\$PATH" >> ~/.zshrc
+source ~/.zshrc
+
+
